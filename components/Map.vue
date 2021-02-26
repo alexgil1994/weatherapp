59de2a0341e1bc1d3e464e5b19760fbc877c496c
alexgil1994 <alexgil2000@hotmail.com>
2021-02-26 23:52:28 +0200

    Fixed installation of vuelayers. Implemented getting geolocation and showing a marker on init load of page.

--- components/Map.vue
+++ components/Map.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <div class="w-full">
+    <div @click="getPixel($event)" class="w-full">
       <client-only>
-        <vl-map class="map" :load-tiles-while-animating="true" :load-tiles-while-interacting="true">
-            <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
+        <vl-map ref="map" class="map" :load-tiles-while-animating="true" :load-tiles-while-interacting="true">
+            <vl-view ref="view" :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation">
+              <vl-geoloc @update:position="geolocPosition = $event">
+                <template slot-scope="geoloc">
+                  <vl-feature v-if="geoloc.position" id="position-feature">
+                    <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
+                    <vl-style-box>
+                      <vl-style-icon src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png" :scale="0.1" :anchor="[0.5, 1]"></vl-style-icon>
+                    </vl-style-box>
+                  </vl-feature>
+                </template>
+              </vl-geoloc>
+            </vl-view>
             <vl-layer-tile id="osm">
                 <vl-source-osm></vl-source-osm>
             </vl-layer-tile>
@@ -20,7 +31,25 @@
         zoom: 5.5,
         center: [2680799.456018, 4615573.515972],
         rotation: 0,
+        geolocPosition: undefined,
+        pixel: {
+          pixelX: '',
+          pixelY: ''
+        }
       }
+    },
+    methods: {
+      getPixel(event) {
+        console.log(event.screenX);
+        console.log(event.screenY);
+        this.pixel.pixelX = event.screenX
+        this.pixel.pixelY = event.screenY
+        console.log(this.geolocPosition)
+        // this.$refs.map.render()
+        // this.$refs.map.refresh()
+        // console.log(this.$refs.map.getCoordinateFromPixel(this.pixel))
+      }
+      
     },
   }
 </script>
--- plugins/vuelayers.js
+++ plugins/vuelayers.js
@@ -1,11 +1,16 @@
+// import Vue from 'vue'
+// import VueLayers from 'vuelayers'
+// import 'vuelayers/lib/style.css' // needs css-loader
+// import {loadingBBox} from 'vuelayers/lib/ol-ext'
+
+// Vue.use(VueLayers)
+// Vue.use(loadingBBox)
+
+// Vue.prototype.$loadingBBox = () => {
+//     return loadingBBox
+// }
 import Vue from 'vue'
 import VueLayers from 'vuelayers'
 import 'vuelayers/lib/style.css' // needs css-loader
-import {loadingBBox} from 'vuelayers/lib/ol-ext'
 
 Vue.use(VueLayers)
-Vue.use(loadingBBox)
-
-Vue.prototype.$loadingBBox = () => {
-    return loadingBBox
-}
